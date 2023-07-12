e3c6d78062ec2a0e68638345423d79e19855f2fa
porkowski <borkowskipatryk10@gmail.com>
2023-07-11 22:01:25 -0400

    Need to figure out how to add new rm buttons and read buttons to nodelist.

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ Book.prototype.addBookToLibrary= function () {
 const book1 = new Book("Eckhart Tolle","The Power Of Now",236,"Yes",1997);
 const book2 = new Book("Sun Tzu","The Art Of War",288,"Yes","221 B.C.E");
 const book3 = new Book("Benjamin Graham", "The Intelligent Investor",640,"No",1949);
-const book4 = new Book("Robert Cecil Martin","Clean Code",464,"No",2008)
+const book4 = new Book("Robert Cecil Martin","Clean Code",464,"No",2008);
 
 //Add pre-loaded books to Library array
 book1.addBookToLibrary();
@@ -64,17 +64,42 @@ myLibrary.forEach((book) => {
 addBooks();
 
 
-//new book form/button
-const newBtn = document.getElementById("newBook");
+//Add singular book function
+function addBook(book) {
 
-newBtn.addEventListener("mousedown", () => {
-    prompt("what is your age?",5);
-}
-)
+        //Create a div/card for each book in my library
+        const bookDiv = document.createElement("div");
+        bookDiv.setAttribute('id',myLibrary.indexOf(book));
+    
+        //Create ul element
+        const propList = document.createElement("ul");
+        bookDiv.appendChild(propList);
+    
+            for(const key in book) {
+                // This conditional to remove any methods added to prototype
+                if (book.hasOwnProperty(key)) {
+    
+                    const bookKey = document.createElement("li");
+                    bookKey.textContent = `${key}: ${book[key]}`;
+                    propList.appendChild(bookKey);
+                };
+            };
+        const rmBtn = document.createElement("button");
+        rmBtn.setAttribute('class',"removeBtn");
+        bookDiv.appendChild(rmBtn);
+    
+        const rdBtn = document.createElement("button");
+        rdBtn.setAttribute('class',"readBtn");
+        bookDiv.appendChild(rdBtn);
+    
+        bookCards.appendChild(bookDiv);
+    
+    };
+    
 
 //grab readBtn and removeBtn to DOM.
 //read Button to switch statu
-const readButton = document.querySelectorAll(".readBtn");
+let readButton = document.querySelectorAll(".readBtn");
 readButton.forEach((button) => {
 
     button.addEventListener("mousedown",(button)=> {
@@ -105,7 +130,7 @@ readButton.forEach((button) => {
 })});
 
 
-const removeButton = document.querySelectorAll(".removeBtn");
+let removeButton = document.querySelectorAll(".removeBtn");
 removeButton.forEach((button) =>    { 
 
     button.addEventListener("mousedown",(button)=> {
@@ -116,3 +141,45 @@ removeButton.forEach((button) =>    {
     //remove from HTML
     divElement.remove();
 })});
+
+
+//new book form/button
+const newBtn = document.getElementById("newBook");
+
+const form = document.getElementById("popupForm");
+
+newBtn.addEventListener("mousedown", () => {
+    //unhide form
+    form.setAttribute('class','formDisplay')
+}
+)
+
+//select radio buttons
+
+const radios = document.getElementsByName("read");
+
+
+
+//On submit, hide form and add new book to myLibrary. Add to cards as well
+const submitFormButton = document.getElementById("submitButton");
+
+function handleForm(event) {
+    event.preventDefault();
+    let radioChoice= "";
+//Run through nodelist of radio controls and see which are checked
+    radios.forEach(radio => {
+        if (radio.checked) {radioChoice = radio.value;}
+    })
+
+    const newBook = new Book(author.value,title.value,pages.value,radioChoice,year.value);
+    myLibrary.push(newBook);
+    addBook(newBook);
+
+//Reload buttons for remove and read since a card was added
+removeButton = document.querySelectorAll(".removeBtn");
+readButton = document.querySelectorAll(".readBtn");
+    form.setAttribute('class','formHidden');
+}
+
+form.addEventListener('submit',handleForm);
+
